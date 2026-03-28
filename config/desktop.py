7c23304f3a20692075dbc1be2010627e43f1eb91
asif-mulani <asif@frappe.io>
2026-03-28 12:15:10 +0530

    Create config/desktop.py file for WMS Tally Integration app

---
+++ config/desktop.py
@@ -0,0 +1,22 @@
+# Configurations for the WMS Tally Integration Frappe App
+
+# This script contains the module configuration and menu items.
+
+
+# Module configuration
+modules = {
+    "WMS Tally Integration": {
+        "color": "#589494",
+        "icon": "octicon octicon-file-directory",
+        "type": "module",
+        "label": _("WMS Tally Integration")
+    }
+}
+
+# Menu items for the application
+
+# Main Menu
+app_name = "WMS Tally Integration"
+
+# Add other menu items if needed
+
